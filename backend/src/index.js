f143c968fd492f46b5e3706dc5cd88d564eb35db
karthik_raj <raj.karthik5566@gmail.com>
2025-03-26 22:25:49 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+// import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js"
+
+const app = express();
+// get the port from dotenv file
+// actual -> cosnt port = process.env.port;
+const PORT = 3000;
+
+app.subscribe("/api/auth" , authRoutes);
+
+
+app.listen(PORT , () =>{
+    console.log("server is running on port 3000");
+})
